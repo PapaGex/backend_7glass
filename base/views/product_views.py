5a0109095018c4e4cf3a879a5c0533016a07f0d2
PapaGex <geeksgeckosandgoodvibes@gmail.com>
2023-11-20 01:29:41 -0500

    almost finished

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from django.core.paginator import Paginator, EmptyPage, PageIsNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.models import User
 from base.models import Product, Review
 from base.products import products
@@ -17,14 +17,14 @@ def getProducts(request):
     if query == None:
         query = ''
 
-    products = Product.objects.filter(name__icontains=query)
+    products = Product.objects.filter(name__icontains=query).order_by('-createdAt')
 
     page = request.query_params.get('page')
     paginator = Paginator(products, 5)
 
     try:
         products = paginator.page(page)
-    except PageIsNotAnInteger: 
+    except PageNotAnInteger: 
         products = paginator.page(1)
     except EmptyPage:
         products = paginator.page(paginator.num_pages)
